c982d8a241df49b498706251a8efe716ebce03cb
Sagar Adhikari <164398516+Sagarhoraa@users.noreply.github.com>
2026-04-23 21:40:23 +0545

    Update doctor model to set default for available field
    Changed 'available' field to have a default value of true.

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-//creating a schema for doctor 
+//creating a schema for doctor model
 
 const doctorSchema = new mongoose.Schema({
     name:{type:String, required:true},
@@ -11,7 +11,7 @@ const doctorSchema = new mongoose.Schema({
     degree:{type:String, required:true},
     experience:{type:String, required:true},
     about:{type:String, required:true},
-    available:{type:Boolean, required:true},
+    available:{type:Boolean, default:true},
     fees:{type:Number, required:true},
     address:{type:Object, required:true},
     date:{type:Number, required:true},
